f9295c07147bac95b50ae8c1b6949b6526cc0550
Mohammed Elsharkawy <moelsharkawy@ejada.com>
2025-11-06 14:14:20 +0300

    Add project files.

---
+++ SolidPrinciples/PaymentMethod.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidPrinciples
+{
+	//Open/Closed Principle (OCP)
+	//----------------using abstract
+	public abstract class PaymentMethod
+	{
+		public double amount { get; set; }
+
+		public abstract void ProcessPayment();
+		public void PaymentAfterVat()
+		{
+			Console.WriteLine($"payment after vat: {amount + amount * 0.15}");
+		}
+	}
+	public class CreditCardPayment : PaymentMethod
+	{
+		public override void ProcessPayment()
+		{
+			Console.WriteLine("----- using CreditCardPayment ------------");
+			Console.WriteLine($"process credit card payment: {amount}");
+		}
+	}
+	public class CryptoPayment : PaymentMethod
+	{
+		public override void ProcessPayment()
+		{
+			Console.WriteLine("----- using CryptoPayment ------------");
+			Console.WriteLine($"Processing crypto payment: {amount}");
+		}
+	}
+
+	public class PayPalPayment : PaymentMethod
+	{
+		public override void ProcessPayment()
+		{
+			Console.WriteLine($"payment using paypal method, the amount: {amount}");
+		}
+	}
+
+	public class PaymentMethod4 : PaymentMethod
+	{
+		public override void ProcessPayment()
+		{
+			Console.WriteLine($"payment using PaymentMethod4 method, the amount: {amount + 20 }");
+		}
+	}
+
+	//Payment Processor is closed for modification, open for extension
+	public class PaymentClientClass()
+	{
+		public void ClientPayment(PaymentMethod paymentMethod, double amount)
+		{
+			paymentMethod.amount = amount;
+			paymentMethod.ProcessPayment();
+			paymentMethod.PaymentAfterVat();
+		}
+	}
+
+
+
+
+	//----------------using interface
+	public interface PaymentMethod2
+	{
+		public void ProcessPayment2(decimal amount);
+	}
+
+	public class CreditCardPayment2 : PaymentMethod2
+	{
+		public void ProcessPayment2(decimal amount)
+		{
+			Console.WriteLine($"process credit card payment (using interface): {amount}");
+		}
+
+	}
+	public class CryptoPayment2 : PaymentMethod2
+	{
+		public void ProcessPayment2(decimal amount)
+		{
+			Console.WriteLine($"Processing crypto payment (using interface): {amount}");
+		}
+	}
+	public class NewPayMentMethod : PaymentMethod2
+	{
+		public void ProcessPayment2(decimal amount)
+		{
+			Console.WriteLine($"New payment method added: {amount}");
+		}
+	}
+	public class ClientPayment()
+	{
+		public void Pay(PaymentMethod2 paymentMethod, decimal amount)
+		{
+			paymentMethod.ProcessPayment2(amount);
+		}
+	}
+
+}
---
+++ SolidPrinciples/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SolidPrinciples;
+
+Console.WriteLine("Solid Principles!...........");
+
+
+//---- open/closed
+//----*using abstract*------
+
+
+var pay=new PaymentClientClass();
+pay.ClientPayment(new CreditCardPayment(), 100);
+pay.ClientPayment(new PaymentMethod4(), 1500);
+pay.ClientPayment(new PayPalPayment(), 2000);
+
+
+
+
+
+//----*using interface*------
+
+var pay2 = new ClientPayment();
+pay2.Pay(new CreditCardPayment2(), 50);
+pay2.Pay(new NewPayMentMethod(), 600);
+
+//var processor2 =new PaymentProcessor2();
+//processor2.ProcessPayment2(new CreditCardPayment2(), 350);
+
+
+Console.WriteLine("----end--------");
